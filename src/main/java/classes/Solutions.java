526636f0e725243d18150efb5481715d86258c9c
mcalhoun133 <mcalhoun133@gmail.com>
2024-03-12 15:18:11 -0700

    Seperate class Solutions

---
+++ src/main/java/classes/Solutions.java
@@ -0,0 +1,19 @@
+package classes;
+
+import java.util.Scanner;
+
+public class Solutions {
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int c = scan.nextInt();
+
+
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+        }
+    }
+
